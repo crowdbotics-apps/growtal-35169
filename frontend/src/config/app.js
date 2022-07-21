0f6baca90106ed5b966ba0a9c8bf4deca74fd81d
Saad bin abid <saad.abid@crwodbotics.com>
2022-07-21 12:05:10 +0500

    minor fixings

--- frontend/src/config/app.js
+++ frontend/src/config/app.js
@@ -1,3 +1,3 @@
-// export const BASE_URL = 'http://6b98-39-53-73-204.ngrok.io';
-export const BASE_URL = 'http://192.168.1.57:8000';
+export const BASE_URL = 'http://2b2d-39-53-71-223.ngrok.io';
+// export const BASE_URL = 'http://192.168.1.57:8000';
 
